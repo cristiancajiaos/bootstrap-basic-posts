89fff8524aa1e804634306882e25897210eaa3d0
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-06 20:21:08 -0400

    Creado elemento main

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {NgbTooltip} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [NgbTooltip],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
